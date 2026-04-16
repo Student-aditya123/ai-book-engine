c5a0aca74c6534ff6b8c4738348db034c73301a4
Aditya Singh <adityasingh606104@gmail.com>
2026-04-16 23:55:02 +0530

    now ready backbone

--- backend/books/models.py
+++ backend/books/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    url = models.URLField()
+    rating = models.CharField(max_length=10, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
 # Create your models here.
